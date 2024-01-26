65d058aa21d917bc4b63fc92238d051fe8c4dfa4
khameleonstorm <khameleonstorm@gmail.com>
2024-01-26 10:29:15 +0100

    auth

--- routes/transactions.js
+++ routes/transactions.js
@@ -53,6 +53,34 @@ router.get('/user/:email', async(req, res) => {
   catch(e){ for(i in e.errors) res.status(500).send({message: e.errors[i].message}) }
 });
 
+// Update a single transaction by ID
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { amount, convertedAmount } = req.body;
+
+  try {
+    if (!amount || !convertedAmount) {
+      return res.status(400).send({ message: "Both amount and convertedAmount are required." });
+    }
+
+    // Find the transaction by ID and update the fields
+    const updatedTransaction = await Transaction.findByIdAndUpdate( id,
+      {
+        $set: {
+          amount,
+          'walletData.convertedAmount': convertedAmount,
+        },
+      }, { new: true }
+    );
+
+    // Check if the transaction was found and updated
+    if (!updatedTransaction) return res.status(404).send({ message: "Transaction not found." })
+
+    res.send({message: "Transaction updated successfully."});
+  } catch (error) { res.status(500).send({ message: "Something went wrong while updating the transaction." })}
+});
+
+
 
 // Delete a transaction
 router.delete('/:id', async (req, res) => {
